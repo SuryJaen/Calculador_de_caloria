5225c908006274fc0dcdb1a27234d04d0cf118ba
Belinda <Belinda@LAPTOP-MVND3O3N>
2023-05-30 11:06:08 -0600

    Add project files.

---
+++ Calculador_de_caloria/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculador_de_caloria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            string peso;
+            float Calculador_de_calorias;
+            Console.WriteLine("Escribe tu nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribir tu peso corporal en kg");
+            peso = Console.ReadLine();
+            Calculador_de_calorias = int.Parse(peso) * 19;
+            Console.WriteLine( Nombre + " usted necesita {0} calorias por dia ", Calculador_de_calorias);
+            Console.Read();
+        }
+    }
+}
